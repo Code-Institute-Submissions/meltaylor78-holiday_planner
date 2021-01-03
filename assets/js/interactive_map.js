9cb94afe79747654917a5cb965b6bfa15d99d8b6
meltaylor78 <meltaylor78@gmail.com>
2021-01-03 12:36:25 +0000

    added JS to populate the first two drop downs for map searching..

--- assets/js/interactive_map.js
+++ assets/js/interactive_map.js
@@ -9,40 +9,34 @@ function initMap() {
 }
 
 /* -_-_-_-_-_-_-_-_-_-_ Dropdown Lists _-_-_-_-_-_-_-_-_-_- */
-var subjectObject = {
-  "Front-end": {
-    "HTML": ["Links", "Images", "Tables", "Lists"],
-    "CSS": ["Borders", "Margins", "Backgrounds", "Float"],
-    "JavaScript": ["Variables", "Operators", "Functions", "Conditions"]    
-  },
-  "Back-end": {
-    "PHP": ["Variables", "Strings", "Arrays"],
-    "SQL": ["SELECT", "UPDATE", "DELETE"]
-  }
-}
-window.onload = function() {
-  var subjectSel = document.getElementById("subject");
-  var topicSel = document.getElementById("topic");
-  var chapterSel = document.getElementById("chapter");
-  for (var x in subjectObject) {
-    subjectSel.options[subjectSel.options.length] = new Option(x, x);
-  }
-  subjectSel.onchange = function() {
-    //empty Chapters- and Topics- dropdowns
-    chapterSel.length = 1;
-    topicSel.length = 1;
-    //display correct values
-    for (var y in subjectObject[this.value]) {
-      topicSel.options[topicSel.options.length] = new Option(y, y);
+
+var accommodation_type = ["Camping", "Caravan Park", "Hotel", "Hostel", "Bed & Breakfast"];
+var region = ["Connacht", "Leinster", "Munster", "Ulster"];
+
+window.onload = function () {
+    var select = document.getElementById("accommodation");
+    for(var i = 0; i < accommodation_type.length;++i){  
+        var option = document.createElement('option');
+        option.text = accommodation_type[i];
+        option.value = accommodation_type[i];
+        select.add(option)
+    };
+    var region_lst = document.getElementById("province");
+    for(var x = 0; x < region.length;++x){  
+        var prov = document.createElement('option');
+        prov.text = region[x];
+        prov.value = region[x];
+        region_lst.add(prov)
     }
-  }
-  topicSel.onchange = function() {
-    //empty Chapters dropdown
-    chapterSel.length = 1;
-    //display correct values
-    var z = subjectObject[subjectSel.value][this.value];
-    for (var i = 0; i < z.length; i++) {
-      chapterSel.options[chapterSel.options.length] = new Option(z[i], z[i]);
-    }
-  }
+};
+
+
+
+
+
+var counties = {
+    "Connacht": ["Galway", "Leitrim", "Mayo", "Roscommon", "Sligo"],
+    "Leinster": ["Carlow", "Dublin", "Kildare", "Kilkenny", "Laois", "Longford", "Louth", "Meath", "Offaly", "Westmeath", "Wexford", "Wicklow" ],
+    "Munster": ["Clare", "Cork", "Kerry", "Limerick", "Tipperary", "Waterford"],
+    "Ulster": ["Antrim", "Armagh", "Cavan", "Donegal", "Down", "Fermanagh", "Derry", "Monaghan", "Tyrone"]
 }
