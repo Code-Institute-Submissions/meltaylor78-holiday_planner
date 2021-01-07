a13f69dc7582a01a0dcf5f9da9f3f143d9afc888
meltaylor78 <meltaylor78@gmail.com>
2021-01-07 21:57:20 +0000

    renamed js file to main.js. added hide/show code for for section. added js code to read the menu selections

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,12 @@
+/* -_-_-_-_-_-_-_-_-_-_ Draw Blank Map _-_-_-_-_-_-_-_-_-_- */
 function initMap() {
     var map = new google.maps.Map(document.getElementById("map"), {
         zoom: 7,
         center: {
             lat: 53.383328,
             lng: -7.5091553
-        }
+        },
+        mapTypeId: 'satellite'
     });
 }
 
@@ -14,22 +16,22 @@ var accommodation_type = ["Camping", "Caravan Park", "Hotel", "Hostel", "Bed & B
 var region = ["Connacht", "Leinster", "Munster", "Ulster"];
 
 window.onload = function () {
-    var select = document.getElementById("accommodation");
+    var select = document.getElementById("accommodation");  // populate the accomadation type list
     for(var i = 0; i < accommodation_type.length;++i){  
         var option = document.createElement('option');
         option.text = accommodation_type[i];
         option.value = accommodation_type[i];
         select.add(option)
     };
-    var region_lst = document.getElementById("province");
+    var region_lst = document.getElementById("province");  // populate the list of provinces
     for(var x = 0; x < region.length;++x){  
         var prov = document.createElement('option');
         prov.text = region[x];
         prov.value = region[x];
         region_lst.add(prov)
     }
+    document.getElementById("formSection").style.display = "none" // Hide the form section on load.
 };
-
 
 var counties = {  
                 "Connacht": ["Galway", "Leitrim", "Mayo", "Roscommon", "Sligo"], 
@@ -52,3 +54,26 @@ document.getElementById("province").addEventListener("change", function(){
     var cnty_drop = document.getElementById("county_lst");
     cnty_drop.classList.add("show");
 })
+/* -_-_-_-_-_-_-_-_-_-_ Render Map _-_-_-_-_-_-_-_-_-_- */
+
+function functionRenderMap(){  
+    let acc_type_selected = document.getElementById("accommodation").value;
+    let province_selected = document.getElementById("province").value; 
+    let county_selected = document.getElementById("county").value; 
+
+    console.log("The accommodation type selected was : " + acc_type_selected);
+    console.log("The province selected was : " + province_selected);
+    console.log("The province county selected was : " + county_selected)
+
+}
+
+/* -_-_-_-_-_-_-_-_-_-_ Form Section _-_-_-_-_-_-_-_-_-_- */
+
+function functionShowHide() {
+  var x = document.getElementById("formSection");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
