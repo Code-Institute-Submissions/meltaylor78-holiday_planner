158aa9386ae9a758977bdb13de0f15d4c64ac5d8
meltaylor78 <meltaylor78@gmail.com>
2021-01-16 22:38:04 +0000

    added a js file with list of locations to be loaded to the map

---
+++ assets/locations/locations.js
@@ -0,0 +1,23 @@
+var locations = [
+{
+   "acc_type" : "Camping & Caravan Park",
+   "prov" : "Connacht",
+   "county" : "Sligo",
+   "address" : "Airport Road,Killaspugbrone, Strandhill, Co. Sligo, F91 W540, Ireland",
+   "lat" : 54.2720352,
+   "lng" : -8.6055212,
+   "name" : "Strandhill Caravan & Camping Park",
+   "place_id" : "ChIJY3q5fm_DXkgRwYqzDXv4pQ8",
+   "comments" : "nice place to stay"},
+ {
+   "acc_type" : "Camping",
+   "prov" : "Connacht",
+   "county" : "Letrim", 
+   "address" : "Corry, Drumkeeran, Co. Leitrim, Ireland",
+   "lat" : 54.1666489,
+   "lng" : -8.062045099999999,
+   "name" : "Pink Apple Orchard - Yurt Glamping",
+   "place_id" : "ChIJGeglitL0XkgR-d7H9pnHdW0",
+   "comments" : "dont like Yurts, build real accomadation"}
+]
+
